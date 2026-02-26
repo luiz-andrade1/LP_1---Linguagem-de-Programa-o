4154ec1bdf486d51fe7d1288155a8f16a8125812
luiz-andrade1 <luizfelipequeirosandrade@gmail.com>
2026-02-26 19:08:09 -0300

    Add files via upload

---
+++ Exercícios/Exercicio_1.java
@@ -0,0 +1,5 @@
+public class Exercicio_1 {
+    public static void main(String []args) {
+        System.out.println("Printar novo texto para atividade de Linguagem de Programação modulo 1"); // prints Hello, World!
+    }
+}
