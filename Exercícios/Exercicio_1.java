1c93232fca4a1cb2fe19c864f06f2caf022dba82
luiz-andrade1 <luizfelipequeirosandrade@gmail.com>
2026-03-04 21:03:56 -0300

    Delete Exercícios/Exercicio_1.java

--- Exercícios/Exercicio_1.java
+++
@@ -1,5 +0,0 @@
-public class Exercicio_1 {
-    public static void main(String []args) {
-        System.out.println("Printar novo texto para atividade de Linguagem de Programação modulo 1")
-    }
-}
